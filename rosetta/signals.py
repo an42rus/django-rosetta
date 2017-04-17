19685ab587ea64b2672db9ea1e1c3f147c1260d2
an42rus <andrey.erunov@gmail.com>
2017-04-18 02:57:42 +0300

    added reload server signal

--- rosetta/signals.py
+++ rosetta/signals.py
@@ -6,3 +6,7 @@ entry_changed = dispatch.Signal(
 post_save = dispatch.Signal(
     providing_args=["language_code","request",]
 )
+
+reload_server = dispatch.Signal(
+    providing_args=["request", ]
+)
